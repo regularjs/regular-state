9445e4c86732b25f1716277715f17c85a43581a5
leeluolee <87399126@163.com>
2018-07-03 16:20:46 +0800

    Update 2018-07-03T08:20:39.444Z

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require("gulp"),
   shell = require("gulp-shell");
 
 
-gulp.task('build',  shell.task(['gitbook  build ./']));
+gulp.task('build',  shell.task(['npm run build']));
 
 gulp.task('deploy', ['build'], function () {
   return gulp.src("_book/**/*.*")
